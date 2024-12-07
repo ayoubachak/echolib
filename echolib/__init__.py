d16d9a5ec6d144998f2c2e5f13c8f6361e03f559
ansAAchak <ayoub.achak@ansys.com>
2024-12-07 15:56:39 +0100

    Bump version: 0.1.5 → 0.2.0

--- echolib/__init__.py
+++ echolib/__init__.py
@@ -4,5 +4,5 @@ from echolib.common.config_manager import config_manager
 from echolib.models.model_manager import model_manager
 from echolib.common.logger import logger
 
-__version__ = "0.1.5"
+__version__ = "0.2.0"
 __all__ = ["config_manager", "model_manager", "logger"]
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="echolib",
-    version="0.1.5",
+    version="0.2.0",
     author="Ayoub Achak",
     author_email="ayoub.achak01@example.com",
     description="An AI-driven library for generating content using HuggingFace and LMStudio models.",
